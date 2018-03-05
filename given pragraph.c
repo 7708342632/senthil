4a2528c80341bc4aa6deb38423df9ea3dab1f06e
7708342632 <35688389+7708342632@users.noreply.github.com>
2018-03-05 16:09:45 +0530

    Create given pragraph.c

---
+++ given pragraph.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+char ch[500],i=0,c=0;
+gets(ch);
+while(ch[i]!=0)
+{
+if(ch[i]=='.')
+{
+  c++;
+}
+  else
+  {
+}
+  i++;
+}
+printf("%d",c);
+}
